15d1b80dd6c3058bac14afd7cb00570b5dbe8f88
Artur Ribeiro Alfa <phoenixrebirth1324@gmail.com>
2023-04-06 14:35:04 -0300

    Add project files.

---
+++ TrianguloOrientacaoObjeto.ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace TrianguloOrientacaoObjeto.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            VerificadorDeTriangulos verificador = new VerificadorDeTriangulos();
+            verificador.ladoA = 10.00;
+            verificador.ladoB = 10.00;
+            verificador.ladoC = 10.00;
+
+            Console.Write($"A verificação sera realizada com os valores:\n" +
+                              $"lado A = {verificador.ladoA}\n" +
+                              $"lado B = {verificador.ladoB}\n" +
+                              $"lado C = {verificador.ladoC}\n" +
+                              $"Sendo o resultado igual a: {verificador.VerificarTriangulo()}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TrianguloOrientacaoObjeto.ConsoleApp/VerificadorDeTriangulos.cs
@@ -0,0 +1,44 @@
+﻿namespace TrianguloOrientacaoObjeto.ConsoleApp
+{
+    internal class VerificadorDeTriangulos
+    {
+        public double ladoA;
+        public double ladoB;
+        public double ladoC;
+
+        public VerificadorDeTriangulos()
+        {
+        }
+
+        private bool VerificarTrinaguloValido()
+        {
+            if (ladoA + ladoB >= ladoC
+             && ladoB + ladoC >= ladoA
+             && ladoC + ladoA >= ladoB)
+                return true;
+            else
+                return false;
+        }
+
+        private string VerificarTipoDeTriangulo()
+        {
+
+            if (ladoA ==  ladoB && ladoB == ladoC)
+                return "Triangulo e equilatero";
+            else if (ladoA ==  ladoB
+                ||   ladoB == ladoC
+                ||   ladoC == ladoA)
+                return "Triangulo e isosceles";
+            else
+                return "Triangulo e escaleno";
+        }
+
+        public string VerificarTriangulo()
+        {
+            if (VerificarTrinaguloValido())
+                return VerificarTipoDeTriangulo();
+            else
+                return "Triangulo e invalido";
+        }
+    }
+}
